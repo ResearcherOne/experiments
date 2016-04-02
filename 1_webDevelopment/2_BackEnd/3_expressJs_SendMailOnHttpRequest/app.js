b8e93e456bebe665e7e9bf1a103f18f8776b4332
ResearcherOne <birkan.kolcu@ozu.edu.tr>
2016-04-02 12:43:53 -0400

    sendMailOnHttpRequest experiment is completed.

--- 1_webDevelopment/2_BackEnd/3_expressJs_SendMailOnHttpRequest/app.js
+++ 1_webDevelopment/2_BackEnd/3_expressJs_SendMailOnHttpRequest/app.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var app = express();
 
-app.get('/nodemcuboard', function (req, res) { //Request coming to "yourdomain.com/nodemcuboard" will be handled.
-  res.send('Hello World!');
-  console.log('Get Request is recieved!');
+var sendGridAPIkey = ""; //your sendgrid API key goes here.
+var sendgrid = require("sendgrid")(sendGridAPIkey);
+var email = new sendgrid.Email();
+
+function sendMail(buttonOwner){
+	email.addTo("birkan.kolcu@ozu.edu.tr");
+	email.setFrom("birkan.kolcu@ozudevelopers.com");
+	email.setSubject("Button is triggered.");
+	email.setHtml(buttonOwner+" pushed to button!");
+	sendgrid.send(email);
+} 
+
+app.get('/mailbutton/:buttonowner', function (request, response) { //Request coming to "yourdomain.com/nodemcuboard" will be handled.
+  var buttonOwner = request.params.buttonowner;
+  response.send('Button is succesfully pressed.');
+  sendMail(buttonOwner);
 });
 
 app.listen(80, function () { //Request that is coming to port 80 will be handled.
